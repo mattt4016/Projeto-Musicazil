1f6cd174aae4abf74faba811b7e959fae2318ffa
mattt4016 <89815690+mattt4016@users.noreply.github.com>
2021-11-09 21:49:15 -0300

    Add files via upload

---
+++ Musicazil/Projeto-Musicazil-master/model/modelUsuario.php
@@ -0,0 +1,34 @@
+<?php
+
+function inserirUsuarioBanco($conexao,$senha,$email){
+
+$opcao = ['cost' => 8];
+
+$senhaCrypto = password_hash($senha, PASSWORD_BCRYPT, $opcao);
+
+$query = "insert into tb_usuario(Email_Usua, Senha_Usua)values('{$email}','{$senhaCrypto}')";
+$resultado = mysqli_query($conexao,$query);
+return $resultado;
+}
+
+function listatudoUsuario($conexao){
+    $query = "select * from tb_usuario";
+    $resultado = mysqli_query($conexao, $query);
+    return $resultado;
+}
+
+function listaUsuarioCodigo ($conexao, $codigoUsu)
+{
+    $query = "select * from tb_usuario where Id_Usua = '{$codigoUsu}'";
+    $resultado = mysqli_query($conexao, $query);
+    return $resultado;
+}
+
+function listaUsuarioEmail ($conexao, $emailUsu)
+{
+    $query = "select * from tb_usuario where Email_Usua like '%{$emailUsu}%'";
+    $resultado = mysqli_query($conexao, $query);
+    return $resultado;
+
+}
+?>
